91db6d7393233f4d21feb48648a6025328f49c02
plevente011230 <p.levente011230@gmail.com>
2022-12-19 12:11:56 +0100

    index php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>Szavazz te is!</h1>
+        <p>Lehetőséges van neked is elmondani a véleményedet fotnos témákról.</p>
+    </body>
+</html>
